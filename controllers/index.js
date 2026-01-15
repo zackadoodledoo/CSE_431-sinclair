ad5633059cd6013a5694ec2b8e17c2914c724322
zackadoodledoo <sin12014@byui.edu>
2026-01-15 11:07:28 -0800

    Week One auditted

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+const awesomeFunction = (req, res, next) => {
+    res.json('I know Briana Sinclair');
+
+}
+
+module.exports = { awesomeFunction };
---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
